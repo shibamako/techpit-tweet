5efd55b17cd6a16199f7bc12691833bec44e3f5c
柴田誠 <makox0105@gmail.com>
2021-08-25 17:44:04 +0900

    参考用

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :tweets, dependent: :destroy
+
+  validates :username, uniqueness: true, length: { in: 1..15 }
+end
